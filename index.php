63969f16cba8c59e9df0d219214b1e2f390e14dd
Pablo Sazycz <pablosazycz@gmail.com>
2022-11-02 02:32:07 -0300

    Cambio de lugar de tabla

--- index.php
+++ index.php
@@ -1,27 +1,27 @@
 <?php
-	$nombre = '';
-	$apellido = '';
+$nombre = '';
+$apellido = '';
 
-	function saludo($nom, $ape)
-	{
-		$nom = $_POST["nom"];
-		$ape = $_POST["ape"];
-		return $nom . ' ' . $ape;
-	}
-$fecha_nac=null;
-	function anios($nac)
-	{
-		$nac = new DateTime($_POST["fnac"]);
-		$hoy = date_create("now");
-		$hoy2 = date_add($hoy, date_interval_create_from_date_string("1 year"));
-		$edad = $hoy2->diff($nac);
-		return ($edad)->format('%y años');
-	}
+function saludo($nom, $ape)
+{
+	$nom = $_POST["nom"];
+	$ape = $_POST["ape"];
+	return $nom . ' ' . $ape;
+}
+$fecha_nac = null;
+function anios($nac)
+{
+	$nac = new DateTime($_POST["fnac"]);
+	$hoy = date_create("now");
+	$hoy2 = date_add($hoy, date_interval_create_from_date_string("1 year"));
+	$edad = $hoy2->diff($nac);
+	return ($edad)->format('%y años');
+}
 
-	$ingreso = date('1990-03-20');
-	$salida = date('2023-03-23');
+$ingreso = date('1990-03-20');
+$salida = date('2023-03-23');
 
-	/* function dias_pasados($fecha_inicial, $fecha_final)
+/* function dias_pasados($fecha_inicial, $fecha_final)
 	{
 
 		$fecha_inicial = date_create($_POST["fnac"]);
@@ -38,14 +38,15 @@ $fecha_nac=null;
 	}
 
  */
-$ip=$_SERVER['REMOTE_ADDR'];
-$apiKey='dae74d852e3b4dd7bf474a4f63634cac';
-$location= get_geolocation($apiKey,$ip);
-$decodedLocation =json_decode($location,true);
-	
+$ip = $_SERVER['REMOTE_ADDR'];
+$apiKey = 'dae74d852e3b4dd7bf474a4f63634cac';
+$location = get_geolocation($apiKey, $ip);
+$decodedLocation = json_decode($location, true);
 
-function get_geolocation($apiKey, $ip, $lang = "en", $fields = "*", $excludes = "") {
-	$url = "https://api.ipgeolocation.io/ipgeo?apiKey=".$apiKey."&ip=".$ip."&lang=".$lang."&fields=".$fields."&excludes=".$excludes;
+
+function get_geolocation($apiKey, $ip, $lang = "en", $fields = "*", $excludes = "")
+{
+	$url = "https://api.ipgeolocation.io/ipgeo?apiKey=" . $apiKey . "&ip=" . $ip . "&lang=" . $lang . "&fields=" . $fields . "&excludes=" . $excludes;
 	$cURL = curl_init();
 
 	curl_setopt($cURL, CURLOPT_URL, $url);
@@ -54,13 +55,13 @@ function get_geolocation($apiKey, $ip, $lang = "en", $fields = "*", $excludes = 
 	curl_setopt($cURL, CURLOPT_HTTPHEADER, array(
 		'Content-Type: application/json',
 		'Accept: application/json',
-		'User-Agent: '.$_SERVER['HTTP_USER_AGENT']
+		'User-Agent: ' . $_SERVER['HTTP_USER_AGENT']
 	));
 
 	return curl_exec($cURL);
 }
 
-	?>
+?>
 
 <!DOCTYPE html>
 <html lang="es">
@@ -82,8 +83,8 @@ function get_geolocation($apiKey, $ip, $lang = "en", $fields = "*", $excludes = 
 	</div>
 
 	<div class="row">
-		<div class="col-4"></div>
-		<div class="col-4 border">
+		<div class="col-3"></div>
+		<div class="col-6 border">
 			<form action="#" method="POST">
 				<div class="mb-3 pt-3">
 					<label id="nom" name="nom" class="form-label">Ingrese su Nombre</label>
@@ -104,12 +105,9 @@ function get_geolocation($apiKey, $ip, $lang = "en", $fields = "*", $excludes = 
 				</div>
 			</form>
 		</div>
-		<div class="col-4"></div>
+		<div class="col-3"></div>
 	</div>
 	<br>
-
-
-
 
 
 	<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -121,51 +119,51 @@ function get_geolocation($apiKey, $ip, $lang = "en", $fields = "*", $excludes = 
 				</div>
 				<div class="modal-body">
 					Hola <?php echo saludo($nombre, $apellido) ?> gracias por visitarnos!
-					Vas a  cumplir <?php echo anios($fecha_nac); ?>!
+					Vas a cumplir <?php echo anios($fecha_nac); ?>!
 					Nos estas visitado de:
-					<div class="container-fluid"> //localizacion por ip
-	<div class="row">
-		<div class="col-12">
-			<h3 class="text-center">Tu Ip es: <?php echo $decodedLocation['ip'] ?> </h3>
-		</div>
-		<br>
-		<hr>
-		<div class="col-12">
-			<table class="table text-center">
-				<tr>
-				<th>Continente</th>
-				<td><?php echo $decodedLocation['continent_name'] ?></td>
-				</tr>
-				<tr>
-				<th>Pais</th>
-				<td><?php echo $decodedLocation['country_name'] ?></td>
-				</tr>
-				<tr>
-				<th>Provincia</th>
-				<td><?php echo $decodedLocation['state_prov'] ?></td>
-				</tr>
-				<tr>
-				<th>Ciudad</th>
-				<td><?php echo $decodedLocation['city'] ?></td>
-				</tr>
-				<tr>
-				<th>Latitud</th>
-				<td><?php echo $decodedLocation['latitude'] ?></td>
-				</tr>
-				<tr>
-				<th>Longitud</th>
-				<td><?php echo $decodedLocation['longitude'] ?></td>
-				</tr>
-				<th>Bandera</th>
-				<td> <img src="<?php echo $decodedLocation['country_flag'] ?>" alt="" srcset=""> </td>
-				</tr>
-				<th>Te conectas con</th>
-				<td> <?php echo $decodedLocation['isp']?> </td>
-				</tr>
-			</table>
-		</div>
-	</div>
-</div>
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-12">
+								<h3 class="text-center">Tu Ip es: <?php echo $decodedLocation['ip'] ?> </h3>
+							</div>
+							<br>
+							<hr>
+							<div class="col-12">
+								<table class="table text-center">
+									<tr>
+										<th>Continente</th>
+										<td><?php echo $decodedLocation['continent_name'] ?></td>
+									</tr>
+									<tr>
+										<th>Pais</th>
+										<td><?php echo $decodedLocation['country_name'] ?></td>
+									</tr>
+									<tr>
+										<th>Provincia</th>
+										<td><?php echo $decodedLocation['state_prov'] ?></td>
+									</tr>
+									<tr>
+										<th>Ciudad</th>
+										<td><?php echo $decodedLocation['city'] ?></td>
+									</tr>
+									<tr>
+										<th>Latitud</th>
+										<td><?php echo $decodedLocation['latitude'] ?></td>
+									</tr>
+									<tr>
+										<th>Longitud</th>
+										<td><?php echo $decodedLocation['longitude'] ?></td>
+									</tr>
+									<th>Bandera</th>
+									<td> <img src="<?php echo $decodedLocation['country_flag'] ?>" alt="" srcset=""> </td>
+									</tr>
+									<th>Te conectas con</th>
+									<td> <?php echo $decodedLocation['isp'] ?> </td>
+									</tr>
+								</table>
+							</div>
+						</div>
+					</div>
 				</div>
 				<div class="modal-footer">
 					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -174,4 +172,5 @@ function get_geolocation($apiKey, $ip, $lang = "en", $fields = "*", $excludes = 
 		</div>
 	</div>
 </body>
+
 </html>
